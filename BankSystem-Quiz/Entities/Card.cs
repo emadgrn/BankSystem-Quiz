0e06e5ff619d5cad28f6acfb15a6b0c4d6ed042c
Emad Gerami <emad.grnd@gmail.com>
2025-10-03 09:52:45 +0330

    init and add Entities

---
+++ BankSystem-Quiz/Entities/Card.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankSystem_Quiz.Entities
+{
+    public class Card
+    {
+        [Key]
+        public string CardNumber { get; set; }
+        public string HolderName { get; set; }
+        public string Password { get; set; }
+        public float Balance { get; set; }
+        public bool IsActive { get; set; }
+        public List<Transaction> SentTransactions { get; set; }
+        public List<Transaction> ReceivedTransactions { get; set; }
+    }
+}
---
+++ BankSystem-Quiz/Entities/Transaction.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankSystem_Quiz.Entities
+{
+    public class Transaction
+    {
+        public int TransactionId { get; set; }
+
+        [ForeignKey("SourceCard")]
+        public string SourceCardNumber { get; set; }
+        public Card SourceCard { get; set; }
+
+        [ForeignKey("DestinationCard")]
+        public string DestinationCardNumber { get; set; }
+        public Card DestinationCard { get; set; }
+        public float Amount { get; set; }
+        public DateTime TransactionDate { get; set; }
+        public bool isSuccessful { get; set; }
+
+
+    }
+}
