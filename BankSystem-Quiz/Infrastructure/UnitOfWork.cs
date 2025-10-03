34851fa10375ae7ff0284e23e38a52197470ca2d
Emad Gerami <emad.grnd@gmail.com>
2025-10-03 10:13:44 +0330

    Add UnitOfWork

---
+++ BankSystem-Quiz/Infrastructure/UnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using BankSystem_Quiz.Infrastructure.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankSystem_Quiz.Infrastructure
+{
+    public class UnitOfWork(AppDbContext _dbContext)
+    {
+        public void Save()
+        {
+            _dbContext.SaveChanges();
+        }
+    }
+}
