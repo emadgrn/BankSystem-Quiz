9308ac61b5209d3d92344532417deb80ef43cbfa
Emad Gerami <emad.grnd@gmail.com>
2025-10-03 10:07:58 +0330

    Add LocalStorage

---
+++ BankSystem-Quiz/Infrastructure/Local/LocalStorage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BankSystem_Quiz.Entities;
+
+namespace BankSystem_Quiz.Infrastructure.Local
+{
+    public static class LocalStorage
+    {
+        public static Card? CurrentCard { get; set; }
+
+    }
+}
