d85c65a38944166e81a98a2fbfed14de4c9ba19c
Emad Gerami <emad.grnd@gmail.com>
2025-10-03 10:44:18 +0330

    Add Transaction repo

---
+++ BankSystem-Quiz/Contracts/Repositories/ITransactionRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BankSystem_Quiz.Entities;
+
+namespace BankSystem_Quiz.Contracts.Repositories
+{
+    public interface ITransactionRepository
+    {
+        public int Create(Transaction transaction);
+        public Transaction GetById(int id);
+        public List<Transaction> GetAll();
+        public void UpdateSuccess(Transaction transaction);
+        public void Delete(int id);
+    }
+}
---
+++ BankSystem-Quiz/Infrastructure/Repositories/TransactionRepository.cs
@@ -0,0 +1,54 @@
+﻿using BankSystem_Quiz.Contracts.Repositories;
+using BankSystem_Quiz.Infrastructure.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BankSystem_Quiz.Entities;
+
+namespace BankSystem_Quiz.Infrastructure.Repositories
+{
+    public class TransactionRepository(AppDbContext _dbContext) : ITransactionRepository
+    {
+        public int Create(Transaction transaction)
+        {
+            _dbContext.Transactions.Add(transaction);
+
+
+            return transaction.TransactionId;
+        }
+
+        public Transaction GetById(int id)
+        {
+            var transaction = _dbContext.Transactions.FirstOrDefault(x => x.TransactionId == id);
+
+            if (transaction is null)
+                throw new Exception($"Transaction with ID {id} is not found");
+
+            return transaction;
+        }
+
+        public List<Transaction> GetAll()
+        {
+            return _dbContext.Transactions
+                .Include(b => b.Category)
+                .ToList();
+        }
+
+        public void UpdateSuccess(Transaction transaction)
+        {
+            var model = GetById(transaction.TransactionId);
+
+            model.isSuccessful = transaction.isSuccessful;
+        }
+
+        public void Delete(int id)
+        {
+            var model = GetById(id);
+
+            _dbContext.Transactions.Remove(model);
+        }
+    }
+
+}
