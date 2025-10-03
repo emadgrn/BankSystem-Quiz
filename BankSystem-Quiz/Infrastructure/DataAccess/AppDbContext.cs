c8c2e9809c1975fca10977d42d2ef40366a5986d
Emad Gerami <emad.grnd@gmail.com>
2025-10-03 10:06:01 +0330

    Create appdbcontext

---
+++ BankSystem-Quiz/Infrastructure/DataAccess/AppDbContext.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BankSystem_Quiz.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BankSystem_Quiz.Infrastructure.DataAccess
+{
+    public class AppDbContext:DbContext
+    {
+        public DbSet<Card> Cards { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .UseSqlServer(
+                    "Server=.\\SQLEXPRESS;Database=BankSystem-Quiz;Trusted_Connection=True;TrustServerCertificate=True;");
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Transaction>()
+                .HasOne(t => t.SourceCard)
+                .WithMany(c => c.SentTransactions)
+                .HasForeignKey(t => t.SourceCardNumber)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Transaction>()
+                .HasOne(t => t.DestinationCard)
+                .WithMany(c => c.ReceivedTransactions)
+                .HasForeignKey(t => t.DestinationCardNumber)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
